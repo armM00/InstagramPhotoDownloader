7e6f1579b37a86b7a8fb6bf056bfa7ce20d5daca
Hardy <freeappsforall.softworks@protonmail.com>
2023-04-19 04:41:18 +0300

    Fixes

--- web.py
+++ web.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-import PIL
 
 st.title("Instagram Photo Downloader")
 st.info("""This Python application downloads a photo from an Instagram post. 
@@ -10,37 +9,39 @@ user_input = st.text_input(label='Enter a URL',
                            placeholder="Paste the URL in https://www.instagram.com/p/CrL8j9cputW/ format",
                            key="user_input")
 
-if len(user_input) == 40:
-    pass
-else:
-    if len(user_input) > 40:
+
+if st.button("Download Photo"):
+    if len(user_input) == 40:
+        url = user_input
+
+    elif len(user_input) > 40:
         url = user_input[:40]
+
     else:
         st.error("Please enter a URL in https://www.instagram.com/p/CrL8j9cputW/ format")
         exit()
 
-    if st.button("Download Photo"):
-        response = requests.get(f"{url}media/?size=l", stream=True)
+    response = requests.get(f"{url}media/?size=l", stream=True)
 
-        with open("instagram.jpg", "wb") as f:
-            f.write(response.content)
+    with open("instagram.jpg", "wb") as f:
+        f.write(response.content)
 
-        st.image('instagram.jpg')
+    st.image('instagram.jpg')
 
-        with open("instagram.jpg", "rb") as f:
-            file_content = f.read()
+    with open("instagram.jpg", "rb") as f:
+        file_content = f.read()
 
-        file_name = 'download.jpg'
+    file_name = 'download.jpg'
 
-        st.download_button(label="Download file", data=file_content, file_name=file_name, mime='image/jpeg')
+    st.download_button(label="Download file", data=file_content, file_name=file_name, mime='image/jpeg')
 
-    # try:
-    #     if len(url) != 40:
-    #         st.error("Please enter a URL in https://www.instagram.com/p/CrL8j9cputW/ format")
-    # except (requests.exceptions.InvalidSchema,
-    #         requests.exceptions.MissingSchema,
-    #         PIL.UnidentifiedImageError):
-    #     # Empty except block to suppress any errors
-    #     pass
-    #
-    #
+# try:
+#     if len(url) != 40:
+#         st.error("Please enter a URL in https://www.instagram.com/p/CrL8j9cputW/ format")
+# except (requests.exceptions.InvalidSchema,
+#         requests.exceptions.MissingSchema,
+#         PIL.UnidentifiedImageError):
+#     # Empty except block to suppress any errors
+#     pass
+#
+#
