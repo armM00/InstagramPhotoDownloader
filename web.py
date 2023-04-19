acfe2d995b7888d6e2b31ea6c46d9ba400cf074a
Hardy <freeappsforall.softworks@protonmail.com>
2023-04-19 05:47:41 +0300

    "Revamped Instagram Photo Downloader with Streamlit.  Now with improved user interface and download functionality.  Get your favorite Instagram photos with ease!"

--- web.py
+++ web.py
@@ -54,3 +54,6 @@ if st.button("Download Photo"):
 
         if st.button("Reload"):
             st.rerun()
+with st.expander("Legal Info"):  # Element 5
+    st.write("<br><a href='https://linktr.ee/arm_andreasian_' style='color:yellow;'>Armen-Jean Andreasian</a> "
+             "<br>Free Apps for All © 2023", unsafe_allow_html=True)
