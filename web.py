0efaba05a5a6b0c75c42f336f5ac336c8bf94de1
Hardy <freeappsforall.softworks@protonmail.com>
2023-04-19 04:12:58 +0300

    Photo Downloader app:
    Added Instagram photo downloader feature
    Implemented photo downloading and saving
    Improved UI and error handling
    Updated requirements for necessary packages
    Refactored code for better maintainability

--- web.py
+++ web.py
@@ -6,11 +6,11 @@ st.info("""This Python application downloads a photo from an Instagram post.
                \nIt takes the URL of the post as input and saves the photo as a JPG file.""")
 
 user_input = st.text_input(label='', placeholder="Paste the URL", key="user_input")
-
+print(len(user_input))
 
 if st.button("Download Photo"):
-    response = requests.get(f"{user_input}media/?size=l", stream=True)
     if len(user_input) == 40:
+        response = requests.get(f"{user_input}media/?size=l", stream=True)
 
         with open("instagram.jpg", "wb") as f:
             f.write(response.content)
