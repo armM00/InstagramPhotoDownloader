e84ceaf49505fdafe4ba2f76d8bf99304c5f3d3e
Hardy <freeappsforall.softworks@protonmail.com>
2023-05-28 05:58:47 +0700

    added a test file. should fix the error

--- web.py
+++ web.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import requests
+import io
 
 icon = 'logo.ico'
-st.set_page_config(page_title="IGD",
-                   page_icon=icon)
-
+st.set_page_config(page_title="IGD", page_icon=icon)
 
 animation = """
 <style>
@@ -24,7 +23,6 @@ animation = """
 }
 </style>
 """
-st.write("Dickj")
 st.markdown(animation, unsafe_allow_html=True)
 
 st.title("Instagram Photo Downloader")
@@ -34,36 +32,23 @@ st.info("""This app downloads a photo from an Instagram post.
 user_input = st.text_input(label='Enter a URL',
                            placeholder="https://www.instagram.com/p/BsOGulcndj-/",
                            key="user_input").strip()
-if user_input:
-    if len(user_input) >= 40:
+
+if st.button("Download Photo") and len(user_input) >= 40:
+    try:
         url = user_input[:40]
-        if st.button("Download Photo"):
-            try:
-                response = requests.get(f"{url}media/?size=l", stream=True)
-                if response.status_code == 200:
-                    with open("instagram.jpg", "wb") as f:
-                        f.write(response.content)
+        response = requests.get(f"{url}media/?size=l", stream=True)
+        if response.status_code == 200:
+            image_bytes = io.BytesIO(response.content)
+            st.image(image_bytes, use_column_width=True)
 
-                    st.image('instagram.jpg')
+            file_content = response.content
+            file_name = 'download.jpg'
 
-                    with open("instagram.jpg", "rb") as f:
-                        file_content = f.read()
-
-                    file_name = 'download.jpg'
-
-                    st.download_button(label="Download file", data=file_content, file_name=file_name, mime='image/jpeg')
-                else:
-                    st.error("Failed to download the photo. Please try again later.")
-            except (requests.HTTPError, requests.ConnectionError) as e:
-                st.error("An error occurred while downloading the photo. Please try again later.")
-                st.error(str(e))
-    else:
-        st.error("Please enter a valid URL")
-
-
-
-
-    #
-    # with st.expander("Legal Info"):  # Element 5
-    #     st.write("<br><a href='https://linktr.ee/arm_andreasian_' style='color:yellow;'>Armen-Jean Andreasian</a> "
-    #              "<br>Free Apps for All © 2023", unsafe_allow_html=True)
+            st.download_button(label="Download file", data=file_content, file_name=file_name, mime='image/jpeg')
+        else:
+            st.error("Failed to download the photo. Please try again later.")
+    except (requests.HTTPError, requests.ConnectionError) as e:
+        st.error("An error occurred while downloading the photo. Please try again later.")
+        st.error(str(e))
+elif len(user_input) < 40:
+    st.error("Please enter a valid URL")
